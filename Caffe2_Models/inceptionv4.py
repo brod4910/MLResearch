134dcfd39b55c568fc6dea9bafbc4a73743eacb2
BrianRod <brod4910@gmail.com>
2018-03-06 12:45:48 -0700

    First half of inception stem implemented

--- Caffe2_Models/inceptionv4.py
+++ Caffe2_Models/inceptionv4.py
@@ -18,27 +18,41 @@ class Inceptionv4():
 	'''
 	Args:
 		model: is the intialized model_helper
-		prev_blop: prev_blob is the data that was taken from the dataset
+		prev_blob: prev_blob is the data that was taken from the dataset
 		sp_batch_norm: is the momentum for spatial batch normalization
 	'''
 	def __init__(self, model, prev_blob, test_flag, sp_batch_norm=.99):
 		self.model = model
 		self.prev_blob = prev_blob
-		self.sp_batch_norm = sp_batch_norm
 		self.test_flag = test_flag
+		self.sp_batch_norm = sp_batch_norm
 
-	def add_conv_layer(self, input_dims, output_dims, name, kernel, stride= 1, pad= 0):
-		self.prev_blob = brew.conv(
-			self.model,
-			self.prev_blob,
-			name,
-			input_dims,
-			output_dims,
-			kernel=kernel,
-			stride=stride,
-			pad=pad,
-			no_bias=1,
-			)
+	def add_conv_layer(self, input_dims, output_dims, name, kernel, stride= 1, pad= 0, prev_blob= None):
+		if prev_blob == None:
+			self.prev_blob = brew.conv(
+				self.model,
+				self.prev_blob,
+				name,
+				input_dims,
+				output_dims,
+				kernel=kernel,
+				stride=stride,
+				pad=pad,
+				no_bias=1,
+				)
+		else:
+			self.prev_blob = brew.conv(
+				self.model,
+				prev_blob,
+				name,
+				input_dims,
+				output_dims,
+				kernel=kernel,
+				stride=stride,
+				pad=pad,
+				no_bias=1,
+				)
+
 		return self.prev_blob
 
 	def add_relu_activ(self):
@@ -47,6 +61,25 @@ class Inceptionv4():
 			self.prev_blob,
 			self.prev_blob,
 			)
+		return self.prev_blob
+
+	def add_max_pool(self, prev_blob, name, kernel, stride):
+		self.prev_blob = brew.max_pool(
+			self.model,
+			prev_blob,
+			name,
+			kernel= kernel,
+			stride= stride,
+			)
+		return prev_blob
+
+	def concat_layers(self, *args, axis=1):
+		self.prev_blob, split_info = brew.concat(
+			args,
+			axis,
+			)
+
+		print(split_info)
 		return self.prev_blob
 
 	def add_sp_batch_norm(self, filters, name):
@@ -61,17 +94,18 @@ class Inceptionv4():
 			)
 		return self.prev_blob
 
-	def create_combined_layers(self, ):
+	def Inception_Stem(self, model):
 
+		self.add_conv_layer(1, 32, 'conv1_stem', 3, stride= 2)
+		self.add_conv_layer(32, 32, 'conv2_stem', 3)
+		self.add_conv_layer(32, 64, 'conv3_stem', 3)
 
-	def Inception_Stem(model, data):
-		brew.conv(model, data, 'conv1_stem', dim_in= 1, dim_out= 32, kernel= 3, stride=2)
-		brew.conv(model, conv1, 'conv2_stem', dim_in= 32, dim_out= 32, kernel= 3)
-		brew.conv(model, conv2, 'conv3_stem', dim_in= 32, dim_out= 64, kernel= 3)
+		# creating conv layer first so we can utilize the prev_blob
+		self.add_conv_layer(64, 96, 'conv4_stem', 3, stride= 2)
 
-		model.net.MaxPool(conv3, 'pool1_stem', kernel=3, stride=2)
-		brew.conv(model, conv3, 'conv4_stem', dim_in= 64, dim_out= 96, kernel= 3, stride=2)
+		self.add_max_pool('conv3_stem', 'pool1_stem', 3, 2)
 
+		self.concat_layers('pool1_stem', 'conv4_stem')
 
 		# conv4 = brew.conv(model, conv3, output_dim= 96, kernel_w= 3, kernel_h= 3, stride_h= 2, stride_w= 2, name='conv4_stem')
 
@@ -127,6 +161,8 @@ if __name__ == '__main__':
 
 	train_model = model_helper.ModelHelper(name="inceptionv4_train", arg_scope=arg_scope)
 
+	inception = Inceptionv4(train_model, X, 0,)
 
+	inception.Inception_Stem(train_model)
 
 	print(workspace.Blobs())
